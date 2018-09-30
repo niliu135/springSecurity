22a022db3a7cd9d2503e0e39c16ea0bc0c2f1b6d
niliu135 <34618694+niliu135@users.noreply.github.com>
2018-09-30 14:50:17 +0800

    添加branch 测试类

---
+++ src/main/java/cn/echisan/springbootjwtdemo/utils/AddBranch1.java
@@ -0,0 +1,11 @@
+package cn.echisan.springbootjwtdemo.utils;
+
+/**
+ * @author : liuhh
+ * @time 2018/9/30  14:48
+ **/
+public class AddBranch1 {
+    public AddBranch1() {
+    }
+    public static final String BRANCH1="BEANCH1";
+}
