07beb9db0054f56eb9b8a6c86ac66b9835590dbb
niliu135 <34618694+niliu135@users.noreply.github.com>
2018-09-30 14:30:47 +0800

    添加test 测试类

---
+++ src/main/java/cn/echisan/springbootjwtdemo/utils/GitAddTest.java
@@ -0,0 +1,13 @@
+package cn.echisan.springbootjwtdemo.utils;
+
+/**
+ * @author : liuhh
+ * @time 2018/9/30  14:25
+ **/
+public class GitAddTest {
+    public GitAddTest() {
+    }
+    public static final String TAGS="test";
+
+
+}
